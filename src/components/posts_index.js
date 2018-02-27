60fb65b27df8c07b91c02ee1c82936bfdf18c72f
jhreinha <joereinhart@shaw.ca>
2018-02-26 19:50:22 -0700

    done bitch

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,19 +1,46 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        //console.log(this.props.posts[0].id);
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={`/posts/${post.id}`}>
+                        {post.title}
+                    </Link>
+                </li>
+            );
+        });
     }
 
     render() {
         return (
             <div>
-                Posts Index
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Write Some Shit
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
